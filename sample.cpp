500c4d341ccba2f874d75e9f382444120b3104c6
Anubhab Sen <anubhabsen@gmail.com>
2017-01-22 18:39:47 +0530

    music added properly

--- sample.cpp
+++ sample.cpp
@@ -1504,50 +1504,38 @@ int main(int argc, char **argv)
 
   initGL(window, width, height);
 
-  //Music
+   mpg123_handle *mh;
+    unsigned char *buffer;
+    size_t buffer_size;
+    size_t done;
+    int err;
 
-  // mpg123_handle *mh;
-  // unsigned char *buffer;
-  // size_t buffer_size;
-  // size_t done;
-  // int err;
+    int driver;
+    ao_device *dev;
 
-  // int driver;
-  // ao_device *dev;
+    ao_sample_format format;
+    int channels, encoding;
+    long rate;
 
-  // ao_sample_format format;
-  // int channels, encoding;
-  // long rate;
+    /* initializations */
+    ao_initialize();
+    driver = ao_default_driver_id();
+    mpg123_init();
+    mh = mpg123_new(NULL, &err);
+    buffer_size = 3000;
+    buffer = (unsigned char*) malloc(buffer_size * sizeof(unsigned char));
 
-  // if(argc < 2)
-  //     exit(0);
+    /* open the file and get the decoding format */
+    mpg123_open(mh, "lav.mp3");
+    mpg123_getformat(mh, &rate, &channels, &encoding);
 
-  // /* initializations */
-  // ao_initialize();
-  // driver = ao_default_driver_id();
-  // mpg123_init();
-  // mh = mpg123_new(NULL, &err);
-  // buffer_size = 64;
-  // buffer = (unsigned char*) malloc(buffer_size * sizeof(unsigned char));
-
-  // /* open the file and get the decoding format */
-  // mpg123_open(mh, argv[1]);
-  // mpg123_getformat(mh, &rate, &channels, &encoding);
-
-  // /* set the output format and open the output device */
-  // format.bits = mpg123_encsize(encoding) * BITS;
-  // format.rate = rate;
-  // format.channels = channels;
-  // format.byte_format = AO_FMT_NATIVE;
-  // format.matrix = 0;
-  // dev = ao_open_live(driver, &format, NULL);
-
-  // /* decode and play */
-  // if (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK)
-  //   ao_play(dev, buffer, done);
-  // else mpg123_seek(mh, 0, SEEK_SET);
-
-  // Music ends
+    /* set the output format and open the output device */
+    format.bits = mpg123_encsize(encoding) * BITS;
+    format.rate = rate;
+    format.channels = channels;
+    format.byte_format = AO_FMT_NATIVE;
+    format.matrix = 0;
+    dev = ao_open_live(driver, &format, NULL);
 
   double last_update_time_brick_form = glfwGetTime(), current_time_brick_form, current_time_laser, last_update_time_laser = glfwGetTime();
   double last_update_time_brick_fall = glfwGetTime(), current_time_brick_fall;
@@ -1558,7 +1546,10 @@ int main(int argc, char **argv)
   while (!glfwWindowShouldClose(window)) {
 
     // OpenGL Draw commands
-
+    if (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK)
+      ao_play(dev, (char *)buffer, done);
+    else
+      mpg123_seek(mh, 0, SEEK_SET);
     draw(window, width, height);
     // draw3DObject draws the VAO given to it using current MVP matrix
 
@@ -1595,5 +1586,11 @@ int main(int argc, char **argv)
     }
   }
   glfwTerminate();
+  free(buffer);
+  ao_close(dev);
+  mpg123_close(mh);
+  mpg123_delete(mh);
+  mpg123_exit();
+  ao_shutdown();
   return 0;
 }
